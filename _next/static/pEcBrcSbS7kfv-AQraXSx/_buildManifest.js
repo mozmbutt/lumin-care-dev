1c3df341c4949d94141fc1bbe9446390357622af
Moazzam B <mozmbutt8@gmail.com>
2023-07-28 20:45:12 +0500

    tetsing node API

---
+++ _next/static/pEcBrcSbS7kfv-AQraXSx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-83f78c7cc0e59e85.js","static/chunks/85-7d445dabe1dcbbcb.js","static/chunks/pages/index-8bf7ac1c00ea8bab.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
